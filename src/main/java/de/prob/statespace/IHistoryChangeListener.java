6f753ac8d74c98b5280b07fe7cb9893fff6a2d11
Jens Bendisposto <jens@bendisposto.de>
2012-09-26 15:17:48 +0200

    removed files

--- src/main/java/de/prob/statespace/IHistoryChangeListener.java
+++
@@ -1,7 +0,0 @@
-package de.prob.statespace;
-
-import de.prob.model.representation.AbstractModel;
-
-public interface IHistoryChangeListener {
-	public void historyChange(History history, AbstractModel model);
-}
